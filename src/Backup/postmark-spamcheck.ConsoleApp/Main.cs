3006ff1e179bdef6160400e740558396aedca59a
Mike Brind <mikebrind@live.co.uk>
2018-10-05 16:56:46 +0100

    Commit all
    Commit all

--- src/Backup/postmark-spamcheck.ConsoleApp/Main.cs
+++ src/Backup/postmark-spamcheck.ConsoleApp/Main.cs
@@ -11,9 +11,7 @@ namespace postmarkspamcheck.ConsoleApp
 	{
 		public static void Main(string[] args)
 		{
-            //Message-Id: <e7c47897-21aa-4441-8ead-31400ba6759b.eml@www.stepex.com>
-
-            var email = System.IO.File.ReadAllText(@"c:\users\mikebrind\desktop\email.txt");
+			var email = Console.ReadLine();
 			var spamChecker = new PostmarkSpamcheck();
 			var score = spamChecker.GetScore(email);
 			if (score.Success)
@@ -33,7 +31,6 @@ namespace postmarkspamcheck.ConsoleApp
 			{
 				Console.WriteLine("Failed: {0}", report.Message);	
 			}
-            Console.ReadLine();
 		}
 	}
 }
